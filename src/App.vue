a64db8dd30e2d5d514ee40f7e32f2943d79f39ec
hirakei1203 <hirano.keitaro.64x@gmail.com>
2022-02-05 22:34:03 +0900

    router code

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <script lang="ts" setup>
-import { reactive } from "vue";
 
-const state = reactive({ name: "" });
-const startGame = () => {
-  alert(`ユーザ名：${state.name}`);
-}
 </script>
 
 <template>
-  <h1>トップ画面</h1>
-  <p>{{ state.name }}</p>
-  <input
-    v-model="state.name"
-    placeholder="名前を入力してください (10文字以内)"
-    maxlength="10"
-  ><br><br>
-  <button v-on:click="startGame()">
-    ゲームスタート！
-  </button>
+  <router-view />
 </template>
 
 <!-- state.name 確認用のタグのスタイルを指定する -->
